04c2d44307945418d2210f1dfe9829126818549d
Felipe Melantonio <felipemelantonio3@gmail.com>
2025-08-01 19:06:43 -0300

    mensagem do que mudou

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Department department = new Department(1, "Human Resources");
+        System.out.println(department);
+        
     }
 }
