5f5b7f926247ccad556dd5564f11d2b890c15416
Felipe Melantonio <felipemelantonio3@gmail.com>
2025-08-01 19:28:38 -0300

    mensagem do que mudou denovo

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Department department = new Department(1, "Human Resources");
         System.out.println(department);
-        
+        Seller seller = new Seller(1, "John Doe","bob@gmail", new Date(),3000.0, department);
+        System.out.println(seller); 
     }
 }
